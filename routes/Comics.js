57fe75876be99ab6a51fe5f89ec1bb3e416b570c
stfDS <dossantos.stefane@gmail.com>
2023-11-19 22:01:58 +0100

    add home and comics routes

--- routes/Comics.js
+++ routes/Comics.js
@@ -3,10 +3,26 @@ const axios = require("axios");
 const express = require("express");
 const router = express.Router();
 
-router.get("/comics", async (req, res) => {
+router.get("/comics/:skip", async (req, res) => {
+  const skip = req.params.skip;
   try {
     const response = await axios.get(
-      `https://lereacteur-marvel-api.herokuapp.com/comics?apiKey=${process.env.MARVEL_API_KEY}`
+      `https://lereacteur-marvel-api.herokuapp.com/comics?skip=${skip}&apiKey=${process.env.MARVEL_API_KEY}`
+    );
+
+    res.json(response.data);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+router.get("/comics/:skip/:title", async (req, res) => {
+  const skip = req.params.skip;
+  const title = req.params.title;
+
+  try {
+    const response = await axios.get(
+      `https://lereacteur-marvel-api.herokuapp.com/comics?skip=${skip}&title=${title}&apiKey=${process.env.MARVEL_API_KEY}`
     );
 
     res.json(response.data);
@@ -17,11 +33,12 @@ router.get("/comics", async (req, res) => {
 
 router.get("/comic/:id", async (req, res) => {
   const comicId = req.params.id;
+  console.log(req.params);
   try {
     const response = await axios.get(
       `https://lereacteur-marvel-api.herokuapp.com/comic/${comicId}?apiKey=${process.env.MARVEL_API_KEY}`
     );
-    console.log(response.data);
+
     res.json(response.data);
   } catch (error) {
     res.status(500).json({ message: error.message });
--- routes/Home.js
+++ routes/Home.js
@@ -3,10 +3,26 @@ const axios = require("axios");
 const express = require("express");
 const router = express.Router();
 
-router.get("/characters", async (req, res) => {
+router.get("/characters/:skip/:name", async (req, res) => {
+  const name = req.params.name;
+  const skip = req.params.skip;
   try {
     const response = await axios.get(
-      `https://lereacteur-marvel-api.herokuapp.com/characters?apiKey=${process.env.MARVEL_API_KEY}`
+      `https://lereacteur-marvel-api.herokuapp.com/characters?skip=${skip}&name=${name}&apiKey=${process.env.MARVEL_API_KEY}`
+    );
+
+    res.json(response.data);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+router.get("/characters/:skip", async (req, res) => {
+  const skip = req.params.skip;
+
+  try {
+    const response = await axios.get(
+      `https://lereacteur-marvel-api.herokuapp.com/characters?skip=${skip}&apiKey=${process.env.MARVEL_API_KEY}`
     );
 
     res.json(response.data);
