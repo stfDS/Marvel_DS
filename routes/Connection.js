8a190b88e3b325fcba5bfb3b5c8f3961bf9846d2
stfDS <dossantos.stefane@gmail.com>
2023-12-23 18:33:13 +0100

    fix cookies

--- routes/Connection.js
+++ routes/Connection.js
@@ -83,7 +83,7 @@ userRouter.post("/login", cors(corsOptions2), async (req, res) => {
       sameSite: "none",
     });
 
-    res.status(200).json({ ...user._doc, password: undefined });
+    res.status(200).json({ ...user._doc, password: undefined, token });
   } catch (err) {
     res.status(500).json({
       message: "An error occurred while connecting",
