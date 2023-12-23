7e2f82dbaac7d12114f899ce336ab1bbe5031aaa
stfDS <dossantos.stefane@gmail.com>
2023-12-23 16:44:00 +0100

    fix cookies

--- middlewares/isAuthenticated.middleware.js
+++ middlewares/isAuthenticated.middleware.js
@@ -6,19 +6,19 @@ const isAuthenticated = async (req, res, next) => {
     const token = req.cookies.jwt;
 
     if (!token) {
-      return res.status(401).json({ message: "Unauthorized" });
+      return res.status(401).json({ message: "Unauthorized, token" });
     }
 
     const { userId } = jwt.verify(token, process.env.JWT_SECRET);
 
     if (!userId) {
-      return res.status(401).json({ message: "Unauthorized" });
+      return res.status(401).json({ message: "Unauthorized , id" });
     }
 
     const user = await User.findById(userId);
 
     if (!user) {
-      return res.status(401).json({ message: "Unauthorized" });
+      return res.status(401).json({ message: "Unauthorized , user" });
     }
 
     req.user = user;
